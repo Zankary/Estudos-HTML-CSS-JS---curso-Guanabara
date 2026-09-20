08e68dd2964dac7878cd1127789852347be7131c
Zankary <rogeroazpg@gmail.com>
2026-09-20 04:59:54 -0300

    Estudos HTML CSS JS - curso Gustavo Guanabara

---
+++ _javascript/funcoes.js
@@ -0,0 +1,3 @@
+function mudaFoto (foto) { //<!-- executar mudaFoto com o parametro (foto) definição abaixo-->
+    document.getElementById("icone").src = foto; //<!--pega o endereço do icone e transforma no src = foto que estiver listado abaixo de acordo com a função q for executada-->
+}
